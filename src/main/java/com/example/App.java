b6112106f69363f05da36cfa77db10e726923ea3
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2024-03-06 11:56:34 -0500

    Add logger dependency

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,20 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		try {
+			LOGGER.info("Hello world...");
+			int x = 1 / 0;
+			System.exit(0);	
+		} catch (Exception e) {
+			LOGGER.error("Exception: {}", e.getMessage(), e);
+			System.exit(1);
+		}
+		
+	}
 }
